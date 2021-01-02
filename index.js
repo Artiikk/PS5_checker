6e7cafe43f4eff721aec0e5d2f9f9420b2f5c4b7
Artiikk <harfild1@ukr.net>
2021-01-02 19:27:53 +0200

    upd

--- index.js
+++ index.js
@@ -33,11 +33,7 @@ const webSitesUrl = [
 
 async function makeScreenshot(url, name) {
   try {
-    const browser = await puppeteer.launch({ 
-      executablePath: '/usr/bin/chromium-browser',
-      headless: false,
-      args: ['--no-sandbox'] 
-    });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url);
     await page.setViewport({ width: 1366, height: 800 });
